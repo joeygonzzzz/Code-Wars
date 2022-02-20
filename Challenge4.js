17f1b088af518734ef1bd56e16c20d8b29c09e29
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-02-20 17:14:17 -0600

    Challenges

---
+++ Challenge4.js
@@ -0,0 +1,15 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, i as vowels
+
+// The input string will only consist of lower case letters and/or spaces
+
+function getCount(str) {
+    var vowelsCount = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u'] {
+        if (vowels.includes(char)) {
+            vowelsCount++
+        }
+    }
+    return vowelsCount;
+}
