fc4df28891473f5551d606fa3218c29cdc128798
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-09-20 22:30:37 -0500

    If boolean then boolean

---
+++ BooleanRewrite.js
@@ -0,0 +1,13 @@
+// In this series we're going to see common redundancies and superfluities that make our code unnecessarily complicated and less readable, and we're going to learn how to avoid them.
+
+// In line with the spirit of the series, we can summarize the general rules of minimalist code in two simple principles:
+
+// Keep your code clean and readable.
+// While not violating the first principle: get rid of everything superfluous.
+// In order to achieve this you should:
+
+// Deepen your knowledge of logics.
+// Deepen your understanding of the particular language you're coding with.
+
+
+const isEven = (n) => n % 2 ===0;
