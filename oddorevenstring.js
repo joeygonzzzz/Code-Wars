f05263b6167eeb1af6bc05e7eab44e3adc0af5c1
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-09-28 22:10:55 -0500

    odd or even string question

---
+++ oddorevenstring.js
@@ -0,0 +1,20 @@
+// Given a string, return true if its length is even or false if the length is odd.
+
+// Examples
+// oddOrEven("apples") ➞ true
+// // The word "apples" has 6 characters.
+// // 6 is an even number, so the program outputs true.
+
+// oddOrEven("pears") ➞ false
+// // "pears" has 5 letters, and 5 is odd.
+// // Therefore the program outputs false.
+
+// oddOrEven("cherry") ➞ true
+
+function oddOrEven(s) {
+	if (s.length % 2 == 0) {
+		return true
+	} else {
+		return false
+	}
+}
