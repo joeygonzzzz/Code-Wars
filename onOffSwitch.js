0e1dd138925539df33232d6b5e98351f703df845
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-10-03 22:07:41 -0500

    Exponent and Math.pow answer

---
+++ onOffSwitch.js
@@ -0,0 +1,3 @@
+// Create a function that returns how many possible arrangements can come from a certain number of switches (on / off). In other words, for a given number of switches, how many different patterns of on and off can we have?
+
+posCom = (num) => Math.pow(2, num)
